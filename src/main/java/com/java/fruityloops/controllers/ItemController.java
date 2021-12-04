7a4987f15d8f8e8cd04ae6ee663b1da7f0279543
Christopher-Teague <cmteague77@gmail.com>
2021-12-03 22:18:11 -0800

    tinkering

--- src/main/java/com/java/fruityloops/controllers/ItemController.java
+++ src/main/java/com/java/fruityloops/controllers/ItemController.java
@@ -20,6 +20,11 @@ public class ItemController {
      fruits.add(new Item("Mango", 2.00));
      fruits.add(new Item("Goji Berries", 4.00));
      fruits.add(new Item("Guava", .75));
+     fruits.add(new Item("Banana", .25));
+     fruits.add(new Item("Pineapple", 3.50));
+     fruits.add(new Item("Apple", 1.25));
+     fruits.add(new Item("Strawberries", 3.75));
+     fruits.add(new Item("Watermelon", 4.25));
      
      // Add fruits your view model here
      
